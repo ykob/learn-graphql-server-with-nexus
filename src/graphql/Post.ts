0edb0b6accbae54ec0be788f3a660c882254d60d
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-08-29 16:01:49 +0900

    added the sample query.

--- src/graphql/Post.ts
+++ src/graphql/Post.ts
@@ -1,8 +1,20 @@
-import { objectType } from "nexus";
+import { objectType, extendType } from "nexus";
 
 export const Post = objectType({
   name: "Post",
   definition(t) {
     t.int("id"), t.string("title"), t.string("body"), t.boolean("published");
+  },
+});
+
+export const PostQuery = extendType({
+  type: "Query",
+  definition(t) {
+    t.nonNull.list.field("drafts", {
+      type: "Post",
+      resolve() {
+        return [{ id: 1, title: "Nexus", body: "...", published: false }];
+      },
+    });
   },
 });

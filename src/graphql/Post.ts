054f52064b72abb6fe9842ddc4f9c7fec91ef853
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-08-29 15:54:25 +0900

    added the sample schema.

---
+++ src/graphql/Post.ts
@@ -0,0 +1,8 @@
+import { objectType } from "nexus";
+
+export const Post = objectType({
+  name: "Post",
+  definition(t) {
+    t.int("id"), t.string("title"), t.string("body"), t.boolean("published");
+  },
+});
--- src/schema.ts
+++ src/schema.ts
@@ -1,12 +1,13 @@
 import { makeSchema } from "nexus";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import * as types from "./graphql/index.ts";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const schema = makeSchema({
-  types: [],
+  types,
   outputs: {
     typegen: join(__dirname, "..", "nexus-typegen.ts"),
     schema: join(__dirname, "..", "schema.graphql"),

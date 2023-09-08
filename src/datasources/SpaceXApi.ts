ba6241772d6782114e50f7155777523eafa4b7a4
Yoichi Kobayashi <yoichi_kobayashi@yayoi-kk.co.jp>
2023-09-08 16:20:02 +0900

    added queries on SpaceX datasource.

--- src/datasources/SpaceXApi.ts
+++ src/datasources/SpaceXApi.ts
@@ -1,9 +1,26 @@
 import { RESTDataSource } from "@apollo/datasource-rest";
 
+type Rocket = {
+  id: string;
+  name: string;
+};
+type Ship = {
+  id: string;
+  name: string;
+};
+
 export class SpaceXApi extends RESTDataSource {
   override baseURL = "https://api.spacexdata.com/v4/";
 
-  async rockets() {
-    return await this.get("rockets");
+  rockets(): Promise<Rocket[]> {
+    return this.get("rockets");
+  }
+
+  rocket(id: string): Promise<Rocket> {
+    return this.get(`rockets/${encodeURIComponent(id)}`);
+  }
+
+  ships(): Promise<Ship[]> {
+    return this.get("ships");
   }
 }
--- src/graphql/spacex.ts
+++ src/graphql/spacex.ts
@@ -1,7 +1,15 @@
-import { extendType, list, nonNull, objectType } from "nexus";
+import { extendType, list, nonNull, objectType, stringArg } from "nexus";
 
 export const Rocket = objectType({
   name: "Rocket",
+  definition(t) {
+    t.nonNull.string("id");
+    t.nonNull.string("name");
+  },
+});
+
+export const Ship = objectType({
+  name: "Ship",
   definition(t) {
     t.nonNull.string("id");
     t.nonNull.string("name");
@@ -14,7 +22,39 @@ export const RocketsQuery = extendType({
     t.field("rockets", {
       type: nonNull(list("Rocket")),
       async resolve(_root, _args, ctx) {
-        return await ctx.dataSources.spaceXApi.rockets();
+        const data = await ctx.dataSources.spaceXApi.rockets();
+
+        return data.map(({ id, name }) => ({
+          id,
+          name,
+        }));
+      },
+    });
+
+    t.field("rocket", {
+      type: nonNull("Rocket"),
+      args: {
+        id: nonNull(stringArg()),
+      },
+      async resolve(_root, args, ctx) {
+        const { id, name } = await ctx.dataSources.spaceXApi.rocket(args.id);
+
+        return {
+          id,
+          name,
+        };
+      },
+    });
+
+    t.field("ships", {
+      type: nonNull(list("Ship")),
+      async resolve(_root, _args, ctx) {
+        const data = await ctx.dataSources.spaceXApi.ships();
+
+        return data.map(({ id, name }) => ({
+          id,
+          name,
+        }));
       },
     });
   },
